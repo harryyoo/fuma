6f2e4e5bee61b4f62a14fafa1a71c218369f8106
liujjxi <liujjxi@qq.com>
2016-09-22 23:32:13 +0800

    login

---
+++ fuping/fis-conf.js
@@ -0,0 +1,19 @@
+// 启用 fis-spriter-csssprites 插件
+fis.match('::package', {
+  spriter: fis.plugin('csssprites')
+});
+fis.match('*.css', {
+  // fis-optimizer-clean-css 插件进行压缩，已内置
+  // optimizer: fis.plugin('clean-css'),
+  // 给匹配到的文件分配属性 `useSprite`
+  useSprite: true
+});
+
+fis.match('*.png', {
+  // fis-optimizer-png-compressor 插件进行压缩，已内置
+  optimizer: fis.plugin('png-compressor')
+});
+fis.match('*.{png,jpg,js,css}', {
+	// useHash: true,
+	release: '/static/$0'
+});
--- fuping/js/ui/login.js
+++ fuping/js/ui/login.js
@@ -1,6 +1,20 @@
+(function ($) {
+
+    //提示插件
+    $.fn.toast =function(msg){
+        var $toast=$('<div class="toast-tip">'+msg+'</div>');
+        $(this).append($toast).find('.toast-tip').fadeIn();
+        setTimeout(function(){
+            $toast.fadeOut(function(){
+                $(this).remove()
+            });
+        },2000);
+    }
+})(jQuery);
+
 $(function() {
 	//背景随机切换
-    var images_id = [["images/pc_login_bg.jpg", "images/pc_login_bg2.jpg"],["images/pc_login_bg3.jpg", "images/pc_login_bg4.jpg"]],
+    var images_id = [["images/pc_login_bg.png", "images/pc_login_bg2.png"],["images/pc_login_bg3.png", "images/pc_login_bg4.png"]],
     	x=0,
     	n=0;
     function switch_background(i) {
@@ -17,4 +31,22 @@ $(function() {
 
         }, 5000);
     }
+    //登录验证
+    var submit=$('.J_submit').on('click',function(){
+        var uiForm=document.form,
+            userName=uiForm.username.value,
+            password=uiForm.password.value,
+            code=uiForm.code.value;
+        
+        if(userName==""||password==""){
+            $('body').toast('用户名或密码不能为空！');
+            return false;
+        }
+    });
+    $('.J_form').on('submit',function(){
+        return submit;
+    })
+
 });
+
+
